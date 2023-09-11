dbc5f7191e5b8901c6e05f297b10dcd9d2f47117
indiraarh <indira.rahmah38@gmail.com>
2023-09-12 02:04:35 +0700

    update upload Tugas_2

---
+++ main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    description = models.TextField()
+    price = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255)
+
