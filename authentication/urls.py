25bdb64a600d85e5ef625781b6c2051a43e7878c
indiraarh <indira.rahmah38@gmail.com>
2023-12-03 13:33:58 +0700

    Tugas 9

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from authentication.views import login
+from authentication.views import login, create_product_flutter, logout
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('auth/', include('authentication.urls')),
     path('create-flutter/', create_product_flutter, name='create_product_flutter'),
     path('logout/', logout, name='logout'),
 ]
